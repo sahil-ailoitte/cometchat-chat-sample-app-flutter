1e916c08f14b349567ada4104244a812217bf33c
nikirana22 <nikirana1983@gmail.com>
2024-02-19 11:52:51 +0530

    refactor(custom_colors): added nextButtonColor color.

--- lib/utils/custom_colors.dart
+++ lib/utils/custom_colors.dart
@@ -4,5 +4,6 @@ class CustomColors {
   static const Color whatsappColor = Color(0xFF008069);
   static const Color messageColor = Color(0xFF889095);
   static const Color mikeColor = Color(0xFF02A884);
-  static const Color textMessageColor =Color(0xFF8798A0);
+  static const Color nextButtonColor = Color(0xFF00A884);
+  static const Color textMessageColor = Color(0xFF8798A0);
 }
