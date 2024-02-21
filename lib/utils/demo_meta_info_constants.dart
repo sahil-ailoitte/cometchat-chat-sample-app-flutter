4bf2af43f91575d3412acf6c3439dc60601fd37d
Ravi Ranjan <ravi@ailoitte.com>
2024-02-21 10:55:33 +0530

    App Released

--- lib/utils/demo_meta_info_constants.dart
+++ lib/utils/demo_meta_info_constants.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class DemoMetaInfoConstants {
   static String name = "Sheikhani Communication";
   static String type = "Demo-App";
-  static String version = "2.7.0";
+  static String version = "2.4.0";
   static String bundle = Platform.isAndroid
       ? "com.sheikhani.communication"
       : "com.sheikhani.communication";
